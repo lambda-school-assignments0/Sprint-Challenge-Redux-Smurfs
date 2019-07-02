32ea3c0a42ea2127b219c8d59ec9e62938af9f53
schang1146 <sammychang2185@gmail.com>
2019-07-01 20:33:18 -0700

    fetching smurfs work

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { getSmurfs, addSmurfs, updateSmurfs, deleteSmurfs } from '../actions';
 /*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
+  to wire this component up you're going to need a few things.
+  I'll let you do this part on your own. 
+  Just remember, `how do I `connect` my components to redux?`
+  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
   render() {
     return (
       <div className="App">
@@ -19,4 +25,16 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  smurfs: state.smurfs,
+  addingSmurfs: state.addingSmurfs,
+  fetchingSmurfs: state.fetchingSmurfs,
+  updatingSmurfs: state.updatingSmurfs,
+  deletingSmurfs: state.deletingSmurfs,
+  error: state.error
+})
+
+export default connect(
+  mapStateToProps,
+  { getSmurfs, addSmurfs, updateSmurfs, deleteSmurfs }
+)(App);
