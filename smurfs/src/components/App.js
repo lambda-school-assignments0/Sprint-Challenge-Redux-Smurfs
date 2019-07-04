61d777554749b7b887b9b210334097cd7e00c64b
schang1146 <sammychang2185@gmail.com>
2019-07-03 21:41:55 -0700

    able to display smurfs from server

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -14,6 +14,7 @@ class App extends Component {
   }
 
   render() {
+    console.log(this)
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
@@ -21,11 +22,7 @@ class App extends Component {
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
         {this.props.smurfs.map(smurf => {
-          <div>
-            <p>{smurf.name}</p>
-            <p>{smurf.age}</p>
-            <p>{smurf.height}</p>
-          </div>
+          return(<p>{smurf.name}</p>)
         })}
       </div>
     );
